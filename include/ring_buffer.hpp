bfbb388e7d17e0a34314c1af5f8edd11a0eaf7b8
kunxl-gg <tiwari.25@iitj.ac.in>
2025-03-05 23:18:13 +0530

    feat: add ring buffer
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

---
+++ include/ring_buffer.hpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+
+class RingBuffer {
+public:
+	/* Constructor for the RingBuffer class
+	 * */
+	RingBuffer();
+
+	/* Destructor for the RingBuffer class
+	 * */
+	~RingBuffer();
+
+	/* Deleting the Copy Constructor of the RingBuffer class
+	 * */
+	RingBuffer(const RingBuffer &ringBuffer) = delete;
+private:
+	uint8_t _capacity = 10;
+};
