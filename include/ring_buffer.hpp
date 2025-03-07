fb2156e7dd529260b6599672e835f663b0c339c2
kunxl-gg <tiwari.25@iitj.ac.in>
2025-03-07 22:53:19 +0530

    feat: implement a ring buffer class
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -1,21 +1,42 @@
+#include <cstddef>
 #include <cstdint>
+#include <atomic>
+#include <vector>
+
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
 
 class RingBuffer {
+/**
+ *
+ *
+ *
+ */
 public:
-	/**
-	 * Constructor for the RingBuffer class
-	 */
-	RingBuffer();
+	// Constructors and Destructors
+	RingBuffer() = default;
+	RingBuffer(const size_t &length);
+	~RingBuffer() = default;
 
-	/**
-	 * Destructor for the RingBuffer class
-	 */
-	~RingBuffer();
+	size_t capacity() const;
+	size_t normalise(size_t ptr) noexcept;
 
-	/**
-	 * Deleting the Copy Constructor of the RingBuffer class
-	 */
-	RingBuffer(const RingBuffer &ringBuffer) = delete;
+	void read() noexcept;
+	bool write(const uint8_t &a, const uint8_t &b) noexcept;
+protected:
+	size_t aquire_read_block();
+	void copy_read_block();
+	void release_read_block();
+
+	size_t aquire_write_block();
+	void copy_write_block();
+	void release_write_block();
 private:
-	uint8_t _capacity = 10;
+	alignas(64) std::atomic<size_t> _rptr;
+	alignas(64) std::atomic<size_t> _wptr;
+	alignas(64) std::atomic<size_t> _size;
+	alignas(64) std::atomic<size_t> _free;
+	alignas(64) std::vector<uint8_t> _buffer;
 };
+
+#endif  // RING_BUFFER_H
---
+++ src/ring_buffer.cpp
@@ -0,0 +1,40 @@
+#include "include/ring_buffer.hpp"
+
+RingBuffer::RingBuffer(const size_t &length) {
+	_buffer.resize(length, 0);
+	_rptr = 0;
+	_wptr = 0;
+	_size = 0;
+	_free = 0;
+}
+
+size_t RingBuffer::capacity() const {
+	return _buffer.size();
+}
+
+size_t RingBuffer::normalise(size_t ptr) noexcept {
+	return ptr < capacity() ? ptr : ptr - capacity();
+}
+
+void RingBuffer::read() noexcept {
+	size_t r = _rptr.load();
+	size_t w = _wptr.load();
+
+	if (w != r)
+		return;
+
+
+}
+
+bool RingBuffer::write(const uint8_t &a, const uint8_t &b) noexcept {
+	size_t w = _wptr.load();
+	size_t r = _rptr.load();
+
+	if (w == r)
+		return false;
+
+	_buffer[w] = a;
+	_buffer[normalise(w + 1)] = b;
+	_wptr.store(normalise(w + 2));
+	return true;
+}
