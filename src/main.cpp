d9dab23197f9acc38a3790fcc3a9f1bae17e6cd6
kunxl-gg <tiwari.25@iitj.ac.in>
2025-03-10 16:57:54 +0530

    fix: add code to read initial config
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,58 @@
+#include <fstream>
+#include <iostream>
 #include "include/pipeline.hpp"
 
+bool readConfig(std::string filename, uint8_t &threshold, float weights[9], int &time) {
+	std::fstream file(filename);
+	if (!file) {
+		std::cerr << "Error in opening file: " << filename << "\n";
+		return false;
+	}
+
+	if (!file >> threshold) {
+		std::cerr << "Error in reading threshold\n";
+		return false;
+	}
+
+	if (!(file >> time)) {
+		std::cerr << "Error in reading time\n";
+		return false;
+	}
+
+	float value;
+	for (int i = 0; i < 9; i++) {
+		if (!(file >> value)) {
+			std::cerr << "Error in reading weight: " << i << "\n";
+			return false;
+		}
+
+		weights[i] = value;
+	}
+
+	return true;
+}
+
 int main() {
-	float weights[9] = { 1.0, 0.5, -1.0, 0.5, 0, -0.5, -1.0, -0.5, 1.0 };
-	Pipeline pipeline(128, weights, 1024);
+	int time;
+	float weights[9];
+	uint8_t threshold;
+	if (!readConfig("config.txt", threshold, weights, time))
+		return 1;
 
+	Pipeline pipeline(threshold, weights, 10);
+
+
+#ifdef _DEBUG
 	pipeline.start();
-
-	std::this_thread::sleep_for(std::chrono::seconds(200));
-
+	std::cout << "Running in _DEBUG mode \n";
+	std::this_thread::sleep_for(std::chrono::seconds(time));
 	pipeline.stop();
+#else
+	std::cout << "Running in Release mode\n";
+	pipeline.start();
+	while (pipeline.should_run());
+	pipeline.stop();
+#endif
 
 	return 0;
 }
