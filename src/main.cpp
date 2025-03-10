b1f1c4906ec399d4fe588311a886279c788940b2
kunxl-gg <tiwari.25@iitj.ac.in>
2025-03-10 09:23:35 +0530

    feat: add pipeline instructions to the main thread
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include "include/pipeline.hpp"
+
 int main() {
+	float weights[9] = { 1.0, 0.5, -1.0, 0.5, 0, -0.5, -1.0, -0.5, 1.0 };
+	Pipeline pipeline(128, weights, 1024);
+
+	pipeline.start();
+
+	std::this_thread::sleep_for(std::chrono::seconds(200));
+
+	pipeline.stop();
+
 	return 0;
 }
